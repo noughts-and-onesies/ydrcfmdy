575cd5e395a2a4aa6fdec127bc2a895fcd7a43eb
deanturpin <git@lvbtn.uk>
2017-08-31 21:51:50 +0100

    Script to generate a rough outline of a song from a list of chords

---
+++ bin/create_song_outline.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+[[ $# == 0 ]] && echo Give me some chords && exit
+
+cat <<HEADER
+X:1
+T: "Song title"
+M: 4/4
+L: 1/4
+K: C
+HEADER
+
+echo -n "|"
+
+for chord in $*; do
+  echo -n " \"$chord\" Z |"
+done
+
+echo
